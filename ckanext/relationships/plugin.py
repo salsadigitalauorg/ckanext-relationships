85d1ec5ced93a9facecd64dc3a729029069c40ad
Mark Calvert <mark.calvert@salsadigital.com.au>
2020-10-22 11:39:10 -0600

    Added 'package_relationship_as_dict' override to fix issue of object not being set because the relationship is to exteranl dataset URL in the comment field

--- ckanext/relationships/plugin.py
+++ ckanext/relationships/plugin.py
@@ -4,12 +4,11 @@ import logging
 
 from ckan.logic import auth_allow_anonymous_access
 from ckan.model.package_relationship import PackageRelationship
-from ckanext.relationships import blueprint, constants, helpers
+from ckanext.relationships import blueprint, constants, helpers, overrides
 from ckanext.relationships.logic.auth import create as auth_create
 from ckanext.relationships.logic.auth import get as auth_get
 from ckanext.relationships.logic.action import create as actions_create, delete as actions_delete, get as actions_get
 from ckan.model.package import Package
-from ckan.model import meta
 
 log = logging.getLogger(__name__)
 
@@ -48,37 +47,11 @@ class RelationshipsPlugin(plugins.SingletonPlugin):
 
     def configure(self, config):
         PackageRelationship.types = constants.RELATIONSHIP_TYPES
-        Package.get = self.qdes_override_package_get
-
-    def qdes_override_package_get(self, reference, for_update=False):
-        '''Returns a package object referenced by its id or name.'''
-
-        if not reference:
-            # Find the callers method name
-            # If it is from 'index_package' return a new initialised package object to prevent a error of trying to retrieve the name attribute from a None object
-            # qdes_ckan-29_lagoon/ckan/ckan_core/ckan/lib/search/index.py Line 224 rel_dict[type].append(model.Package.get(rel['object_package_id']).name)
-            import inspect
-            calling_method = ''
-            try:
-                # TODO: Need to investigate potentially a better way of finding key names instead of using indexes
-                calling_method = inspect.stack()[1][3]
-            except Exception as e:
-                log.error('qdes_override_package_get - inspect.stack: {0}'.format(e))
-
-            if calling_method == 'index_package':
-                return Package()
-            else:
-                return None
-
-        q = meta.Session.query(Package)
-        if for_update:
-            q = q.with_for_update()
-        pkg = q.get(reference)
-        if pkg == None:
-            pkg = Package.by_name(reference, for_update=for_update)
-        return pkg
+        PackageRelationship.as_dict = overrides.package_relationship_as_dict
+        Package.get = overrides.package_get
 
     # IActions
+
     def get_actions(self):
         return {
             'package_relationship_create': actions_create.package_relationship_create,
