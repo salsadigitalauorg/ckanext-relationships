5a699fcc372cbe7740e19bb9ed46a24119cd95ca
awang setyawan <awang@salsadigital.com.au>
2022-01-03 10:39:58 +0700

    [SUPDESQ-29] - added relationship uri

--- ckanext/relationships/constants.py
+++ ckanext/relationships/constants.py
@@ -15,3 +15,30 @@ RELATIONSHIP_TYPES = [
     (u'Is Alternate Of', u'Is Alternate Of'),
     (u'Is Specialization Of', u'Is Generalization Of'),
 ]
+
+RELATIONSHIP_TYPE_URIS = {
+    'unspecified relationship': None,
+    'Is Part Of': 'https://vocabs.gsq.digital/object?uri=http%3A//purl.org/dc/terms/isPartOf',
+    'Has Part': 'https://vocabs.gsq.digital/object?uri=http%3A//purl.org/dc/terms/hasPart',
+    'Is Format Of': 'https://vocabs.gsq.digital/object?uri=http%3A//purl.org/dc/terms/isFormatOf',
+    'Has Format': 'https://vocabs.gsq.digital/object?uri=http%3A//purl.org/dc/terms/hasFormat',
+    'Is Version Of': 'https://vocabs.gsq.digital/object?uri=http%3A//purl.org/dc/terms/isVersionOf',
+    'Has Version': 'https://vocabs.gsq.digital/object?uri=http%3A//purl.org/dc/terms/hasVersion',
+    'Replaces': 'https://vocabs.gsq.digital/object?uri=http%3A//purl.org/dc/terms/replaces',
+    'Is Replaced By': 'https://vocabs.gsq.digital/object?uri=http%3A//purl.org/dc/terms/isReplacedBy',
+    'References': 'https://vocabs.gsq.digital/object?uri=http%3A//purl.org/dc/terms/references',
+    'Is Referenced By': 'https://vocabs.gsq.digital/object?uri=http%3A//purl.org/dc/terms/isReferencedBy',
+    'Requires': 'https://vocabs.gsq.digital/object?uri=http%3A//purl.org/dc/terms/requires',
+    'Is Required By': 'https://vocabs.gsq.digital/object?uri=http%3A//purl.org/dc/terms/isRequiredBy',
+    'Was Derived From': 'https://vocabs.gsq.digital/object?uri=http%3A//www.w3.org/ns/prov%23wasDerivedFrom',
+    'Was Derivation of': 'https://vocabs.gsq.digital/object?uri=https%3A//linked.data.gov.au/def/dataset-relationships/hadDerivation',
+    'Was Influenced By': 'https://vocabs.gsq.digital/object?uri=http%3A//www.w3.org/ns/prov/%23wasInfluencedBy',
+    'Influenced': 'https://vocabs.gsq.digital/object?uri=http%3A//www.w3.org/ns/prov/%23influenced',
+    'Quoted As': 'https://vocabs.gsq.digital/object?uri=https%3A//linked.data.gov.au/def/dataset-relationships/quotedAs',
+    'Quoted From': 'https://vocabs.gsq.digital/object?uri=http%3A//www.w3.org/ns/prov/%23wasQuotedFrom',
+    'Was Revision Of': 'https://vocabs.gsq.digital/object?uri=http%3A//www.w3.org/ns/prov/%23wasRevisionOf',
+    'Had Revision': 'https://vocabs.gsq.digital/object?uri=https%3A//linked.data.gov.au/def/dataset-relationships/hadRevision',
+    'Is Alternate Of': 'https://vocabs.gsq.digital/object?uri=http%3A//www.w3.org/ns/prov/%23alternateOf',
+    'Is Specialization Of': 'https://vocabs.gsq.digital/object?uri=http%3A//www.w3.org/ns/prov/%23specializationOf',
+    'Is Generalization Of': 'https://vocabs.gsq.digital/object?uri=https%3A//linked.data.gov.au/def/dataset-relationships/generalizationOf'
+}
